41bfb852ca2eb6248037850014c3364e2edb7cf6
yhidetoshi <hyajima.1014@gmail.com>
2017-09-05 13:58:56 +0900

    Adding Gorouting

---
+++ Tour_of_Go/Goroutines/default-selection.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(300 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("boom")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}

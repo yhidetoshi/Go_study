8ee6add93dd5684f5630635d833d4a8737555a8d
Hidetoshi Yajima <hyajima.jaist.job@gmail.com>
2017-09-26 23:49:51 +0900

    Create test10-1.go

---
+++ Paiza/test10-1.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	Struckout = 3
+)
+
+func judge(_count_strike, _count_ball int) {
+
+	if _count_strike == Struckout {
+		fmt.Println("out!")
+	} else {
+		fmt.Println("fourball!")
+	}
+}
+
+func main() {
+
+	var throw_balls, i int
+	var str string
+	var count_strike, count_ball int
+
+	fmt.Scan(&throw_balls)
+	ball_type := make([]string, throw_balls)
+
+	for i = 0; i < len(ball_type); i++ {
+
+		fmt.Scan(&str)
+
+		switch str {
+
+		case "strike":
+			count_strike++
+		case "ball":
+			count_ball++
+		}
+		ball_type[i] = str
+
+		if i != throw_balls-1 {
+			fmt.Println(ball_type[i] + "!")
+		}
+
+	}
+	judge(count_strike, count_ball)
+}

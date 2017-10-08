e5cbbd5d5579254b0668bf16343edfba746d2f7d
Hidetoshi Yajima <hyajima.jaist.job@gmail.com>
2017-10-08 19:41:39 +0900

    Update create-aws-credential.go

--- tools/file/create-aws-credential.go
+++ tools/file/create-aws-credential.go
@@ -18,12 +18,31 @@ func fileError(err error){
 	}
 }
 
+
+
+func outputFiles(_lineIn int, _buffIn [N]string){
+	var lineOut int
+	var outFilename string
+
+	for lineOut = 0; lineOut < _lineIn; lineOut++ {
+
+		if lineOut != 0 {
+			outFilename = "output"+strconv.Itoa(lineOut)+".csv"
+			file, err := os.Create(outFilename)
+			file.Write(([]byte)(_buffIn[0]+"\n"))
+			file.Write(([]byte)(_buffIn[lineOut]))
+
+			fileError(err)
+			defer file.Close()
+		}
+	}
+}
+
 func main() {
 	var fp *os.File
 	var err error
-	var lineIn, lineOut int
+	var lineIn int
 	var buffIn [N]string
-	var outFilename string
 
 	// Input File
 	if len(os.Args) < 1 {
@@ -37,7 +56,6 @@ func main() {
 	// 1行ずつファイルを読み込む
 	scanner := bufio.NewScanner(fp)
 	for scanner.Scan() {
-
 		buffIn[lineIn] = scanner.Text()
 		fmt.Println(scanner.Text())
 		lineIn ++ //行数インクリメント
@@ -45,18 +63,6 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
-
-	// 2行目以降をファイル出力する
-	for lineOut = 0; lineOut < lineIn; lineOut++ {
-
-		if lineOut != 0 {
-			outFilename = "output"+strconv.Itoa(lineOut)+".csv"
-			file, err := os.Create(outFilename)
-			file.Write(([]byte)(buffIn[0]+"\n"))
-			file.Write(([]byte)(buffIn[lineOut]))
-
-			fileError(err)
-			defer file.Close()
-		}
-	}
+	// ファイル行数と行毎のデータを配列で渡す
+	outputFiles(lineIn, buffIn)
 }

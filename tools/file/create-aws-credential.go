18583cd0798a2d7a7f14c3019ac2e0d2c5c781e8
Hidetoshi Yajima <hyajima.jaist.job@gmail.com>
2017-10-09 13:21:06 +0900

    Update create-aws-credential.go

--- tools/file/create-aws-credential.go
+++ tools/file/create-aws-credential.go
@@ -6,12 +6,16 @@ import (
 	"os"
 	"strconv"
 	"log"
-)
-const (
-	N = 10
-	INFILE = "./credentials.csv"
+	"archive/zip"
+	"io"
 )
 
+const (
+	N = 10 							// 読み込み行数の最大値
+	INFILE = "./credentials.csv"    // 読み込みcsvファイル名を指定
+)
+
+// エラー処理
 func fileError(err error){
 	if err != nil {
 		log.Fatal("Error", err)
@@ -19,23 +23,39 @@ func fileError(err error){
 }
 
 
-
+// CSVファイルを作成する
 func outputFiles(_lineIn int, _buffIn [N]string){
 	var lineOut int
 	var outFilename string
+	var outFilenameZip string
 
+	// 2行目以降の行数だけファイルを作成(ユーザ毎にファイル作成)
 	for lineOut = 0; lineOut < _lineIn; lineOut++ {
 
 		if lineOut != 0 {
-			outFilename = "output"+strconv.Itoa(lineOut)+".csv"
+			outFilename = "./output"+strconv.Itoa(lineOut)+".csv"
 			file, err := os.Create(outFilename)
 			file.Write(([]byte)(_buffIn[0]+"\n"))
 			file.Write(([]byte)(_buffIn[lineOut]))
 
 			fileError(err)
 			defer file.Close()
+
+			// File(csv)をzip化する
+			outFilenameZip = outFilename+".zip"
+			outFilenameZip, err := os.Create(outFilenameZip)
+			fileError(err)
+
+			zipWriter := zip.NewWriter(outFilenameZip)
+			defer zipWriter.Close()
+
+			if err := makeZip(outFilename, zipWriter); err != nil {
+				panic(err)
+			}
 		}
+
 	}
+
 }
 
 func main() {
@@ -65,4 +85,20 @@ func main() {
 	}
 	// ファイル行数と行毎のデータを配列で渡す
 	outputFiles(lineIn, buffIn)
+}
+
+// File名を渡してzip化する
+func makeZip(filename string, zipWriter *zip.Writer) error{
+
+	src, err := os.Open(filename)
+	fileError(err)
+	defer src.Close()
+
+	writer, err := zipWriter.Create(filename)
+	fileError(err)
+
+	_, err = io.Copy(writer, src)
+	fileError(err)
+
+	return nil
 }

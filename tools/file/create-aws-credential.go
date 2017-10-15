5b41f8d09d63390b974c7433adc0a50b70601e9f
Hidetoshi Yajima <hyajima.jaist.job@gmail.com>
2017-10-15 14:46:17 +0900

    補足コメントと constで定数化した。

--- tools/file/create-aws-credential.go
+++ tools/file/create-aws-credential.go
@@ -2,40 +2,41 @@ package main
 
 import (
 	"bufio"
+	"crypto/rand"
+	"encoding/binary"
 	"fmt"
+	"github.com/yeka/zip"
+	"io"
+	"log"
 	"os"
 	"strconv"
-	"log"
-	//"archive/zip"
-	//"io"
-	"github.com/yeka/zip"
-	//"bytes"
-	//"io"
-	//"bytes"
-	"io"
-	"encoding/binary"
-	"crypto/rand"
 )
 
 const (
-	N = 10 							// 読み込み行数の最大値
-	INFILE = "./credentials.csv"    // 読み込みcsvファイル名を指定
-	PWFILE = "zip-password.txt"
+	N       = 10                  // 読み込み行数の最大値
+	INFILE  = "./credentials.csv" // 読み込みcsvファイル名を指定
+	OUTFILE = "outCredentials_"
+	CSV     = ".csv"
+	ZIP     = ".zip"
+	PWFILE  = "zip-password.txt"
 )
 
-func fileError(err error){
+/* Error処理 */
+func fileError(err error) {
 	if err != nil {
 		log.Fatal("Error", err)
 	}
 }
 
-func genRand() string {
+/* zipのパスワードを発行 */
+func genPWrand() string {
 	var n uint64
 	binary.Read(rand.Reader, binary.LittleEndian, &n)
 	return strconv.FormatUint(n, 36)
 }
 
-func createPWFile(pw string){
+/* zipパスワードファイルを作成 */
+func createPWFile(pw string) {
 	file, err := os.Create(PWFILE)
 	fileError(err)
 	defer file.Close()
@@ -43,28 +44,27 @@ func createPWFile(pw string){
 	file.Write(([]byte)(pw))
 }
 
-func outputFiles(_lineIn int, _buffIn [N]string){
-	var lineOut int
-	var outFilename string
-	var outFilenameZip string
+func outputFiles(_lineIn int, _buffIn [N]string) {
+	var lineOut int           // 出力ファイルの行数カウンタ
+	var outFilename string    // ユーザ毎に分けた個別ファイル名(csv)
+	var outFilenameZip string // ユーザ毎に分けた個別ファイル名(zip)
 
-	// パスワード付きzipのパスワードファイルを生成
-	password := genRand()
-	createPWFile(password)
+	password := genPWrand() // ランダムPWの生成
+	createPWFile(password)  // PWを記入したテキストファイルを生成
 
 	for lineOut = 0; lineOut < _lineIn; lineOut++ {
 
 		if lineOut != 0 {
-			outFilename = "./output"+strconv.Itoa(lineOut)+".csv"
-			file, err := os.Create(outFilename)
-			file.Write(([]byte)(_buffIn[0]+"\n"))
+			outFilename = "./" + OUTFILE + strconv.Itoa(lineOut) + CSV
+			file, err := os.Create(outFilename) // func Create(name string) (*File, error)
+			file.Write(([]byte)(_buffIn[0] + "\n"))
 			file.Write(([]byte)(_buffIn[lineOut]))
 
 			fileError(err)
 			defer file.Close()
 
-			outFilenameZip = outFilename+".zip"
-			outFilenameZip, err := os.Create(outFilenameZip)
+			outFilenameZip = outFilename + ZIP
+			outFilenameZip, err := os.Create(outFilenameZip) // func Create(name string) (*File, error)
 			fileError(err)
 
 			zipWriter := zip.NewWriter(outFilenameZip)
@@ -77,14 +77,14 @@ func outputFiles(_lineIn int, _buffIn [N]string){
 	}
 }
 
-
+/* PW付きZipを作成する */
 func makeZip(filename string, zipWriter *zip.Writer, pw string) error {
 
 	src, err := os.Open(filename)
 	fileError(err)
 	defer src.Close()
 
-	dest, err := zipWriter.Encrypt(filename, pw, zip.AES256Encryption)
+	dest, err := zipWriter.Encrypt(filename, pw, zip.AES256Encryption) // PW付きzipにする
 	fileError(err)
 
 	_, err = io.Copy(dest, src)
@@ -93,32 +93,29 @@ func makeZip(filename string, zipWriter *zip.Writer, pw string) error {
 	return nil
 }
 
-
 func main() {
 	var fp *os.File
 	var err error
-	var lineIn int
-	var buffIn [N]string
+	var lineIn int       // 読み込むファイル(csv)の行数カウンタ
+	var buffIn [N]string // 行毎のデータを格納用
 
-	if len(os.Args) < 1 {
-		fp  = os.Stdin
+	if len(os.Args) < 1 { // os.Args[0]はファイル読込み
+		fp = os.Stdin
 	} else {
 		//fp, err = os.Open(INFILE)
-		fp, err = os.Open(os.Args[1])
+		fp, err = os.Open(os.Args[1]) // func Open(name string) (*File, error)
 		fileError(err)
 		defer fp.Close()
 	}
 
-	scanner := bufio.NewScanner(fp)
-	for scanner.Scan() {
-		buffIn[lineIn] = scanner.Text()
+	scanner := bufio.NewScanner(fp) // func NewScanner(r io.Reader) *Scanner
+	for scanner.Scan() {            // func (s *Scanner) Scan() bool
+		buffIn[lineIn] = scanner.Text() // func (s *Scanner) Text() string
 		fmt.Println(scanner.Text())
-		lineIn ++
+		lineIn++
 	}
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
 	outputFiles(lineIn, buffIn)
 }
-
-

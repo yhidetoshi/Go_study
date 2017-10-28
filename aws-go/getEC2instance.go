1ddb81934af905e2144c1f225c29dd5210986f0f
Hidetoshi Yajima <hyajima.jaist.job@gmail.com>
2017-10-28 15:18:00 +0900

    Update getEC2instance.go

--- aws-go/getEC2instance.go
+++ aws-go/getEC2instance.go
@@ -12,21 +12,21 @@ func main() {
 
 	svc := ec2.New(session.New(), &aws.Config{Region: aws.String("ap-northeast-1")})
 
-
 	// Call the DescribeInstances Operation
 	resp, err := svc.DescribeInstances(nil)
 	if err != nil{
 		panic(err)
 	}
+
 	fmt.Println("インスタンス合計数", len(resp.Reservations))
 
 	for index, _ := range resp.Reservations {
 		//fmt.Println(len(res.Instances))
 
 		for _, inst := range resp.Reservations[index].Instances{
-			fmt.Println("インスタンスID", *inst.InstanceId)
-			//fmt.Println(*inst.PublicIpAddress)
+			fmt.Print("インスタンス情報\t" , *inst.InstanceId, "\t", *inst.ImageId, "\t", *inst.PrivateIpAddress, "\t", *inst.SubnetId, "\t", *inst.InstanceType, "\n")
+
 		}
 	}
-	
+
 }

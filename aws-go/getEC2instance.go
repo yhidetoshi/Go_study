f73424e882d8487b37a8a930c41bc7d95bcee527
Hidetoshi Yajima <hyajima.jaist.job@gmail.com>
2017-10-28 14:40:10 +0900

    Update getEC2instance.go

--- aws-go/getEC2instance.go
+++ aws-go/getEC2instance.go
@@ -1,31 +1,32 @@
+
 package main
 
 import (
 	"fmt"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
 func main() {
-	// Create an EC2 service object in the "us-west-2" region
-	// Note that you can also configure your region globally by
-	// exporting the AWS_REGION environment variable
+
 	svc := ec2.New(session.New(), &aws.Config{Region: aws.String("ap-northeast-1")})
+
 
 	// Call the DescribeInstances Operation
 	resp, err := svc.DescribeInstances(nil)
-	if err != nil {
+	if err != nil{
 		panic(err)
 	}
+	fmt.Println("インスタンス合計数", len(resp.Reservations))
 
-	// resp has all of the response data, pull out instance IDs:
-	fmt.Println("> Number of reservation sets: ", len(resp.Reservations))
-	for idx, res := range resp.Reservations {
-		fmt.Println("  > Number of instances: ", len(res.Instances))
-		for _, inst := range resp.Reservations[idx].Instances {
-			fmt.Println("    - Instance ID: ", *inst.InstanceId)
+	for index, _ := range resp.Reservations {
+		//fmt.Println(len(res.Instances))
+
+		for _, inst := range resp.Reservations[index].Instances{
+			fmt.Println("インスタンスID", *inst.InstanceId)
+			//fmt.Println(*inst.PublicIpAddress)
 		}
 	}
+	
 }

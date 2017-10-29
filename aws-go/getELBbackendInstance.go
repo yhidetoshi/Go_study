15c2890776197e822997b8497de11f65efe747f7
Hidetoshi Yajima <hyajima.jaist.job@gmail.com>
2017-10-29 13:08:54 +0900

    Create getELBbackendInstance.go

---
+++ aws-go/getELBbackendInstance.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/elb"
+)
+
+func main(){
+	elbName := "beaconnect-lb-1"
+	var inserviceInstances []string
+
+	svc := elb.New(session.New(), &aws.Config{Region: aws.String("ap-northeast-1")})
+	params := &elb.DescribeInstanceHealthInput{
+		LoadBalancerName: aws.String(elbName),
+	}
+
+	resELBInfo, err := svc.DescribeInstanceHealth(params)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := range resELBInfo.InstanceStates {
+		result := *resELBInfo.InstanceStates[i].InstanceId + ":" + *resELBInfo.InstanceStates[i].State
+		inserviceInstances = append(inserviceInstances, result)
+	}
+
+	fmt.Println(inserviceInstances)
+}

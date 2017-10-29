f4b3b1a12b29a984acbd8c4f2a06025a4fd194d3
Hidetoshi Yajima <hyajima.jaist.job@gmail.com>
2017-10-29 15:20:27 +0900

    Create elb-deregisterInstance.go

---
+++ aws-go/elb-deregisterInstance.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/elb"
+)
+
+func main(){
+	fmt.Println(os.Args)
+
+	if len(os.Args) <= 2{
+		fmt.Println("引数エラー")
+		os.Exit(1)
+	}
+
+	elbName := os.Args[1]
+	instanceID := os.Args[2]
+
+	svc := elb.New(session.New(), &aws.Config{Region: aws.String("ap-northeast-1")})
+	//func (c *ELB) DeregisterInstancesFromLoadBalancer(input *DeregisterInstancesFromLoadBalancerInput) (*DeregisterInstancesFromLoadBalancerOutput, error)
+
+
+	input := &elb.DeregisterInstancesFromLoadBalancerInput{
+		Instances: []*elb.Instance{
+			{
+				InstanceId: aws.String(instanceID),
+			},
+		},
+		LoadBalancerName: aws.String(elbName),
+	}
+
+	resELBInfo, err := svc.DeregisterInstancesFromLoadBalancer(input)
+	if err != nil {
+		os.Exit(1)
+	}
+	fmt.Println(resELBInfo)
+
+
+}

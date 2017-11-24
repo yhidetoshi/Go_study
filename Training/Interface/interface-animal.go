e823da94b8ed1a3e50cb7c53db545995325dcfce
yhidetoshi <hyajima.1014@gmail.com>
2017-11-24 22:20:56 +0900

    interfaceの学習サンプルを追加

---
+++ Training/Interface/interface-animal.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Animal interface{
+	Cry()
+}
+
+type Dog struct {}
+func (d *Dog) Cry(){
+	fmt.Println("Wanwan")
+}
+
+type Cat struct {}
+func (c *Cat) Cry(){
+	fmt.Println("Nya-nya")
+}
+
+
+func MakeAnimalCry(a Animal) {
+	fmt.Println("DoCry!")
+	a.Cry()
+}
+
+func main() {
+	dog := new(Dog)
+	cat := new(Cat)
+
+	MakeAnimalCry(dog)
+	MakeAnimalCry(cat)
+}

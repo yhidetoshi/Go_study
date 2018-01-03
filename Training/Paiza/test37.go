b59e15d090aa3c6e1ec033ee1abac602193216b4
Hidetoshi Yajima <hyajima.jaist.job@gmail.com>
2018-01-03 13:36:48 +0900

    Update test37.go

--- Training/Paiza/test37.go
+++ Training/Paiza/test37.go
@@ -10,21 +10,22 @@ const (
 
 func main() {
 
-	var pullN int                  // おみくじを引く回数
-	var n1, n2, n3, n4, n5, n6 int //選んだ番号を入力
-	var a1, a2, a3, a4, a5, a6 int //当選番号入力
+	var pullN int                                                      // おみくじを引く回数
+	var pickNum1, pickNum2, pickNum3, pickNum4, pickNum5, pickNum6 int //選んだ番号を入力
+	var a1, a2, a3, a4, a5, a6 int                                     //当選番号入力
 	hitCnt := 0
 
-	fmt.Scan(&n1, &n2, &n3, &n4, &n5, &n6)
+	fmt.Scan(&pickNum1, &pickNum2, &pickNum3, &pickNum4, &pickNum5, &pickNum6)
 	fmt.Scan(&pullN) // おみくじを引く回数を入力
 
-	inputNum := [N]int{n1, n2, n3, n4, n5, n6}
+	inputNum := [N]int{pickNum1, pickNum2, pickNum3, pickNum4, pickNum5, pickNum6}
 	result := make([]int, pullN)
 
 	// 当選確認
 	for i := 0; i < pullN; i++ {
-		fmt.Scan(&a1, &a2, &a3, &a4, &a5, &a6)
-		hitNum := [6]int{a1, a2, a3, a4, a5, a6}
+		fmt.Scan(&a1, &a2, &a3, &a4, &a5, &a6) // 当選番号入力
+		hitNum := [N]int{a1, a2, a3, a4, a5, a6}
+
 		for j := 0; j < N; j++ {
 			for k := 0; k < N; k++ {
 				if inputNum[j] == hitNum[k] {

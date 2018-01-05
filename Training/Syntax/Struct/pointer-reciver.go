969c18a3c071db23447d2c1de3ef030571dc91c2
Hidetoshi Yajima <hyajima.jaist.job@gmail.com>
2018-01-05 17:14:47 +0900

    Update pointer-reciver.go

--- Training/Syntax/Struct/pointer-reciver.go
+++ Training/Syntax/Struct/pointer-reciver.go
@@ -13,15 +13,17 @@ func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-
-func (v *Vertex) Scale(f float64){
+func (v *Vertex) Scale(f float64) {
 	v.X = v.X * f
 	v.Y = v.Y * f
 }
 
-
 func main() {
 	v := Vertex{3, 4}
 	v.Scale(10)
-	fmt.Println(v.Abs())
+	fmt.Println(v.Abs()) 
+  /* 
+  *Vertex -> 50 
+   Vertex -> 5
+  */
 }

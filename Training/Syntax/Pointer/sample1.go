4131a71ba97164219a6a5871c4b0fbaadf36cfa1
Hidetoshi Yajima <hyajima.jaist.job@gmail.com>
2018-02-04 14:25:40 +0900

    Update sample1.go

--- Training/Syntax/Pointer/sample1.go
+++ Training/Syntax/Pointer/sample1.go
@@ -2,17 +2,16 @@ package main
 
 import "fmt"
 
-func main() {
+func main(){
+  var pointer *int // アドレスを格納できるようにしている [*]
+  var n int = 100  
+  pointer = &n
 
-	var pointer *int
-	var n int = 100
-	pointer = &n
 
-	fmt.Println("nのアドレス", &n)
-	fmt.Println("pointer値", pointer)
+  fmt.Println("nのアドレス", &n)       // nのアドレス 0xc420016090 <アドレスの場所を指している>
+  fmt.Println("pointerの値", pointer) // pointerの値 0xc420016090 <アドレスの場所を表示している>
 
-	fmt.Println("nの値", n)
-	fmt.Println("pointerの中身", *pointer)
+  fmt.Println("nの値",n)                 // nの値 100
+  fmt.Println("pointerの中身", *pointer) //pointerの中身 100
 
 }
-

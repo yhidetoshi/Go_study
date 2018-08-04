07536b84a80807896fda0505b6d860ba1901c3e7
Hidetoshi Yajima <hyajima.1014@gmail.com>
2018-08-04 21:20:29 +0900

    Create marshal.go

---
+++ Training/Syntax/Json/marshal.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+  "encoding/json"
+  "fmt"
+  "io/ioutil"
+)
+
+// jsonデコード用に構造体を定義
+type Person struct {
+  Id        int     `json:"id"`
+  Name      string  `json:"name"`
+  Birthday  string  `json:"birthday"`
+}
+
+func main(){
+  bytes, err := ioutil.ReadFile("input-data.json")
+  if err != nil {
+    fmt.Println("err")
+  }
+  // jsonデコード
+  var persons []Person
+  if err := json.Unmarshal(bytes, &persons); err != nil {
+        fmt.Println("err")
+  }
+
+  for _, p := range persons{
+    fmt.Printf("%d : %s`\n", p.Id, p.Name)
+  }
+
+}

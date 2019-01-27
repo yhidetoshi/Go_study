6503d49150408e4df8a39883be9cdf4e2e119602
Hidetoshi Yajima <hyajima.1014@gmail.com>
2019-01-27 09:16:17 +0900

    Update using_close.go

--- Training/Syntax/Goroutine/using_close.go
+++ Training/Syntax/Goroutine/using_close.go
@@ -5,10 +5,10 @@ import (
 	"time"
 )
 
-func recive(name string, ch <-chan int) {
+func receive(name string, ch <-chan int){
 	for {
 		i, ok := <-ch
-		// 受信できるか確認
+		fmt.Println(ok)
 		if ok == false {
 			break
 		}
@@ -19,17 +19,45 @@ func recive(name string, ch <-chan int) {
 
 func main() {
 	ch := make(chan int, 10)
-
-	go recive("1st goroutine", ch)
-	go recive("2nd goroutine", ch)
-	go recive("3rd goroutine", ch)
+	go receive("1st goroutine", ch)
+	go receive("2nd goroutine", ch)
+	go receive("3rd goroutine", ch)
 
 	i := 0
-	for i < 10 {
+	for i < 10{
 		ch <- i
 		i++
+
 	}
 	close(ch)
-
-	time.Sleep(3 * time.Second)
+	time.Sleep(3* time.Second)
 }
+
+/* 実行結果
+true
+true
+1st goroutine 0
+true
+1st goroutine 3
+true
+1st goroutine 4
+true
+true
+3rd goroutine 2
+true
+3rd goroutine 6
+true
+1st goroutine 5
+true
+1st goroutine 8
+true
+1st goroutine 9
+false
+1st goroutineis done
+2nd goroutine 1
+false
+2nd goroutineis done
+3rd goroutine 7
+false
+3rd goroutineis done
+*/

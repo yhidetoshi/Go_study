c9e12514b9238700e151769d255f8b1179f834e5
Hidetoshi Yajima <hyajima.1014@gmail.com>
2019-04-21 22:31:50 +0900

    Update Embeded_dev.go

--- Training/Syntax/Interface/Embeded_dev.go
+++ Training/Syntax/Interface/Embeded_dev.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 type Speak interface {
-    Hello()
-    
+	Hello()
+	
 }
 
 type A struct {
-    Speak
-    B
+	Speak
+	B
 }
 
 type B struct {
@@ -20,14 +20,20 @@ type B struct {
 
 
 func (a *A) Hello() {
-    fmt.Printf("hello %s", a.word)
+	fmt.Printf("hello %s\n", a.word)
+	a.word = "hoge"
 }
 
 func main() {
-    a := &A{}
-    a.word = "world"
-    a.Hello()
+	a := &A{}
+	a.word = "world"
+	a.Hello()
+	fmt.Println(a.word)
 }
 
+
 // 実行結果
-//hello world
+/*
+hello world
+hoge
+*/
